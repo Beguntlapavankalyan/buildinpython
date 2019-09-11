c71d8ffe82d093cfa6de2e0084529ffc583a2664
Beguntlapavankalyan <beguntlapavan@gmail.com>
2019-09-11 10:03:57 +0530

    first commit

---
+++ webapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request,'home.html')
+def about_view(request):
+    return render(request,'about.html')
+def services_view(request):
+    return render(request,'services.html')
